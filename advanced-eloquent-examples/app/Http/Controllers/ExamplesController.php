2b5b557cfa7f8ea20624b1eab1c2a5ce0bc23da8
Yavor Mihaylov <yavor.st.m@gmail.com>
2017-03-08 01:26:23 +0200

    Add Laravel for projects examples

---
+++ advanced-eloquent-examples/app/Http/Controllers/ExamplesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExamplesController extends Controller
+{
+    public function index()
+    {
+        die('here');
+    }
+}
